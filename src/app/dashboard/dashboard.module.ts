27e746ab65b4e0ec3dbcdffe3098884de01dc367
weirdyang <wy4786@gmail.com>
2021-05-23 02:13:27 +0800

    adding shared module, restructuring folders

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,50 +1,39 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MiniCardComponent } from '../shared/mini-card/mini-card.component';
+import { MiniCardComponent } from './mini-card/mini-card.component';
 import { DashboardChartModule } from '../charts/dashboard-chart.module';
 import { DashboardComponent } from './dashboard.component';
-import { CardComponent } from '../shared/card/card.component';
 import { OrdersTableComponent } from '../order/orders-table.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { MatChipsModule } from '@angular/material/chips';
-import { ComponentWrapperComponent } from '../shared/component-wrapper/component-wrapper.component';
 import { WrapperDirective } from '../shared/wrapper.directive';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
     MiniCardComponent,
     DashboardComponent,
-    CardComponent,
     OrdersTableComponent,
     MiniCardComponent,
-    ComponentWrapperComponent,
-    WrapperDirective,
   ],
   imports: [
     CommonModule,
     DashboardChartModule,
     MatToolbarModule,
     MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
     MatListModule,
     MatGridListModule,
-    MatCardModule,
-    MatMenuModule,
     MatPaginatorModule,
     MatChipsModule,
     MatSortModule,
     MatTableModule,
+    SharedModule,
   ],
   exports: [
     DashboardChartModule,
