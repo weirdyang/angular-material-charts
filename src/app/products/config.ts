bdf9af706fb82788eb42456b352cb0bfa58aa648
weirdyang <wy4786@gmail.com>
2021-05-26 22:58:57 +0800

    formatting

--- src/app/products/config.ts
+++ src/app/products/config.ts
@@ -1,3 +1,3 @@
 export const defaultColor: string = 'cool';
 
-export const hours: string[] = ['No Production', 'Good Products Time', 'Downtime', 'Planned Downtime']
+export const hours: string[] = ['Maintenance', 'Production', 'Downtime', 'Scheduled']
