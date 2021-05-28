dc6542cf104dcac68342a2dc3e62212d3e9abfff
weirdyang <wy4786@gmail.com>
2021-05-28 13:41:36 +0800

    fixing console errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HandsetService } from './core/services/handset.service';
 import { ThemeService } from './core/services/theme.service';
 
 @Component({
   selector: 'cd-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
   isDarkTheme = this.themeService.isDarkTheme;
